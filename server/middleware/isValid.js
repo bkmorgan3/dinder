96b6a958df9ab0e489d71d3fe7292d4bbcd465de
brad morgan <bkmorgan3@gmail.com>
2020-06-18 11:03:23 -0700

    updates auth flow for username login

--- server/middleware/isValid.js
+++ server/middleware/isValid.js
@@ -3,7 +3,6 @@
 module.exports = function (req, res, next) {
   // destructure req.body
   const { email, username, password } = req.body;
-  console.log(email, username, password)
 
   // regex check for proper email format
   function validEmail(userEmail) {
@@ -12,17 +11,14 @@ module.exports = function (req, res, next) {
 
   // check for no missing values on signup
   if (req.path === "/signup") {
-    console.log('sss', !email.length);
     if (![email, username, password].every(Boolean)) {
       return res.status(403).json("Missing Credentials");
     } else if (!validEmail(email)) {
       return res.json("Invalid Email");
     }
   } else if (req.path === "/login") {
-    if (![email, password].every(Boolean)) {
+    if (![username, password].every(Boolean)) {
       return res.json("Missing Credentials");
-    } else if (!validEmail(email)) {
-      return res.json("Invalid Email");
     }
   }
 
